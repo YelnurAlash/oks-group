7e33395ebc9dc6504cf74fec07c90bae5392aca6
Yelnur <zhazityelnur@gmail.com>
2023-10-09 01:33:17 +0600

    added get data function

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const json = await fetch('http://localhost:1337/api/properties?populate=*');
+    const data = await json.json();
+    setData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [data])
+
   return (
     <div className="App">
-     test
+      {console.log(data)};
     </div>
   );
 }
